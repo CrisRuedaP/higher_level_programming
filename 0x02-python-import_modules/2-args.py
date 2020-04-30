548b21e6238bf8436128dcccdea19084fb45bbbe
CrisRuedaP <crisruedap@gmail.com>
2020-04-30 21:44:04 +0000

    fixed requeriment

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,9 +3,9 @@ if __name__ == "__main__":
     from sys import argv
     count = len(argv[1:])
     if count == 0:
-        print("0 arguments.")
+        print("{} arguments.".format(count))
     elif count == 1:
-        print("1 argument:")
+        print("{} argument:".format(count))
     else:
         print("{} arguments:".format(count))
     for i in range (1, count + 1):
