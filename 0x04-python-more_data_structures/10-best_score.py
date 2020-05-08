d79ec54b93644a22ecdee8ffc502b253fa6947fb
CrisRuedaP <crisruedap@gmail.com>
2020-05-08 00:48:36 +0000

    added a for statement

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,6 +2,6 @@
 def best_score(a_dictionary):
     if a_dictionary is None:
         return None
-    else:
+    for k in a_dictionary:
         max_score = max(a_dictionary, key=a_dictionary.get)
         return max_score
