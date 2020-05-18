990353ff82b811fa224f7222ee71d82252f158ec
CrisRuedaP <crisruedap@gmail.com>
2020-05-18 23:00:16 +0000

    changed position of variable

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -2,8 +2,8 @@
 def list_division(my_list_1, my_list_2, list_length):
    new_list = []
    for i in range(list_length):
+      result = 0
       try:
-         result = 0
          result = my_list_1[i]/ my_list_2[i]
       except TypeError:
          print("wrong type")
