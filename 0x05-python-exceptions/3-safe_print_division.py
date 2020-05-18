c2e8f4099e57b5527c48cadbbdb2e212e659cd0c
CrisRuedaP <crisruedap@gmail.com>
2020-05-18 21:58:36 +0000

    added return function

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,7 +2,9 @@
 def safe_print_division(a, b):
     try:
         result = a/b
+        print()
     except ZeroDivisionError:
         result = None
     finally:
         print("Inside result: {}".format(result))
+        return result
