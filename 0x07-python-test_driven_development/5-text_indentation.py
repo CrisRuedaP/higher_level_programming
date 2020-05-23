d4b2629d960ea68bfb50fbcaaa51f86854616132
CrisRuedaP <crisruedap@gmail.com>
2020-05-23 06:19:40 +0000

    printing a text with 2 new lines after a characters

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+function that prints a text with 2 new lines
+after each of characters: ., ? and :
+
+"""
+
+
+def text_indentation(text):
+    """ prints a text
+
+    Args:
+        text(str): must be a string
+
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    jump = '\n'
+    quote = '?' + jump
+    dot = '.' + jump
+    doubleDot = ':' + jump
+
+    p = text.replace('?', quote)
+    x = p.replace('.', dot)
+    z = x.replace(':', doubleDot)
+
+    splitted = z.split(jump)
+    lenght = len(splitted)
+    lastLine = splitted[lenght - 1]
+
+    i = 0
+    while i < lenght - 1:
+        print(splitted[i].strip() + jump)
+        i += 1
+    print(lastLine.strip(), end="")
