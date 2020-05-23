08fc20e9171705f2c020e8dca417ad6fdc8ba1c5
CrisRuedaP <crisruedap@gmail.com>
2020-05-23 05:12:23 +0000

    cast when one of the two datatype is a float

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -23,8 +23,8 @@ def add_integer(a, b=98):
         raise TypeError("a must be an integer")
     if b is None:
         raise TypeError("b must be an integer")
-    if not isinstance(a, int):
+    if not isinstance(a, int) or isinstance(a, float):
         raise TypeError("a must be an integer")
-    if not isinstance(b, int):
+    if not isinstance(b, int) or isinstance(b, float):
         raise TypeError("b must be an integer")
     return int(a) + int(b)
