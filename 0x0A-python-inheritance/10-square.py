9de7e4598809738d3d713ec7e212342247b48a9d
CrisRuedaP <crisruedap@gmail.com>
2020-06-03 03:39:14 +0000

    added documentation

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -10,6 +10,7 @@ class Square(Rectangle):
     A class Square that inherits from Rectangle
     """
     def __init__(self, size):
+        """initialize the method"""
         self.integer_validator("size", size)
         self.__size = size
         super().__init__(self.__size, self.__size)
