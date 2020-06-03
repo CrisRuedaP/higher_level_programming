bc137e6a154c2f9988a261adc77c041c96b25332
CrisRuedaP <crisruedap@gmail.com>
2020-06-03 03:35:49 +0000

    changed implementation of area

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -10,15 +10,6 @@ class Square(Rectangle):
     A class Square that inherits from Rectangle
     """
     def __init__(self, size):
-        """
-        initialize the square object
-        """
         self.integer_validator("size", size)
-        super().__init__(size, size)
         self.__size = size
-
-    def area(self):
-        """
-        method that returns the area of the square
-        """
-        return self.__size * self.__size
+        super().__init__(self.__size, self.__size)
