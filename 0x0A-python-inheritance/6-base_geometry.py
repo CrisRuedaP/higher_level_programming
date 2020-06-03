ba279e08af2183c66d7a66eeacdcc028bb7602d8
CrisRuedaP <crisruedap@gmail.com>
2020-06-03 00:11:56 +0000

    defines a Public instance method

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Creates a class BaseGeometry"""
+
+
+class BaseGeometry:
+    def area(self):
+        """
+        Public instance method that raises an exception
+        """
+        raise Exception("area() is not implemented")
