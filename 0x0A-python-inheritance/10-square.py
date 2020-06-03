081fe7bda85c48759c5d996189256bd15130e2f6
CrisRuedaP <crisruedap@gmail.com>
2020-06-03 03:51:19 +0000

    fixed noseque

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Import Rectangle and define a square class
+Import Rectangle and define a square class.
 """
 Rectangle = __import__('9-rectangle').Rectangle
 
@@ -12,5 +12,8 @@ class Square(Rectangle):
     def __init__(self, size):
         """initialize the method"""
         self.integer_validator("size", size)
+        super().__init__(size, size)
         self.__size = size
-        super().__init__(self.__size, self.__size)
+
+    def area(self):
+        return self.__size * self.__size
