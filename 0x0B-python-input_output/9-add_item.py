8ab0c936dafd6b3ace2c8d9292e791c6e8d09729
CrisRuedaP <crisruedap@gmail.com>
2020-06-04 22:49:37 +0000

    added blank-spaces

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -2,16 +2,14 @@
 """
 script that adds all arguments to a Python list, and then save them to a file
 """
-
-
-import json
 from sys import argv
 
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
+
 try:
-    args = load_from_json_file("add.item.json")
+    args = load_from_json_file("add_item.json")
 except:
     args = []
 for item in argv[1:]:
