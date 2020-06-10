c62cab41638acadca0aa198a45f2b139b31f9451
CrisRuedaP <crisruedap@gmail.com>
2020-06-10 01:05:06 +0000

    undo changes

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -55,6 +55,6 @@ class Base:
     @classmethod
     def create(cls, **dictionary):
         """returns an instance with all attributes already set"""
-        dummy = cls(2, 2, 2)
+        dummy = cls(1, 2)
         dummy.update(**dictionary)
         return dummy
