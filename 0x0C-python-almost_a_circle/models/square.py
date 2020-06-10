60d09a135f2f1da984e9ed65f94cc98084332c7b
CrisRuedaP <crisruedap@gmail.com>
2020-06-10 21:10:45 +0000

    corrected output

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -12,8 +12,8 @@ class Square(Rectangle):
 
     def __str__(self):
         """overloading method"""
-        return ("[square] ({}) {}/{} - {}".format(self.id, self.x,
-                                                  self.y, self.width))
+        return ("[square] ({}) {}/{} - {}".
+                format(self.id, self.x, self.y, self.width))
 
     @property
     def size(self):
