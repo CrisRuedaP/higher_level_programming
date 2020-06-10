8b17721759fb6f7ea84931890dcbf94a9d5520d1
CrisRuedaP <crisruedap@gmail.com>
2020-06-10 16:02:53 +0000

    added more tests cases

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -17,3 +17,38 @@ class Square_Tests(unittest.TestCase):
         pep8style = pep8.StyleGuide(quite=True)
         result = pep8style.check_files(['./models/square.py'])
         self.assertEqual(result.total_errors, 0)
+
+    def test_S_integer_size(self):
+        """integer validator"""
+        with self.assertRaisesRegex(TypeError, "width must be an integer"):
+            r = Square("10")
+        with self.assertRaisesRegex(ValueError, "width must be > 0"):
+            r = Square(-10)
+        with self.assertRaisesRegex(ValueError, "width must be > 0"):
+            r = Square(0)
+        with self.assertRaisesRegex(TypeError, "width must be an integer"):
+            r = Square({})
+        with self.assertRaisesRegex(TypeError, "width must be an integer"):
+            r = Square(10.1)
+
+    def test_S_integer_x(self):
+        """integer validator"""
+        with self.assertRaisesRegex(TypeError, "x must be an integer"):
+            r = Square(10, "2")
+        with self.assertRaisesRegex(ValueError, "x must be >= 0"):
+            r = Square(10, -2)
+        with self.assertRaisesRegex(TypeError, "x must be an integer"):
+            r = Square(10, {})
+        with self.assertRaisesRegex(TypeError, "x must be an integer"):
+            r = Square(10, 2.1)
+
+    def test_S_integer_y(self):
+        """integer validator"""
+        with self.assertRaisesRegex(TypeError, "y must be an integer"):
+            r = Square(10, 3, "2")
+        with self.assertRaisesRegex(ValueError, "y must be >= 0"):
+            r = Square(10, 3, -2)
+        with self.assertRaisesRegex(TypeError, "y must be an integer"):
+            r = Square(10, 3, {})
+        with self.assertRaisesRegex(TypeError, "y must be an integer"):
+            r = Square(10, 3, 2.1,)
