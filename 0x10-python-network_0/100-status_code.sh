00dd21d1111770804d50cc2016bc3f32887ec7bb
CrisRuedaP <crisruedap@gmail.com>
2020-08-18 21:44:49 +0000

    Changed order again

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#Get the HTTP status code of a URL passed
-curl -sI -o /dev/null -w "%{http_code}" "$1"
+# Get the HTTP status code of a URL passed
+curl "$1" -sI -o /dev/null -w "%{http_code}"
