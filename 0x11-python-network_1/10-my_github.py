05ddd261a7bc891998dd2c9b10f0be2e5cfc8f8d
CrisRuedaP <crisruedap@gmail.com>
2020-08-24 23:18:33 +0000

    fixed outpu

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -8,9 +8,9 @@ import sys
 
 
 if __name__ == "__main__":
-    response = requests.get(
-        'https://api.github.com/user', auth=(sys.argv[1], sys.argv[2]))
+    url = "https://api.github.com/user"
+    response = requests.get(url, auth=(sys.argv[1], sys.argv[2]))
     try:
         print(response.json().get("id"))
     except:
-        pass
+        print("None")
