b123629b0ba97c9205def595341ca21fe15d3d57
CrisRuedaP <crisruedap@gmail.com>
2020-08-24 22:20:18 +0000

    discarding previous changes

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
-""" fetch from given URL """
+"""fetch from given URL"""
 import urllib.request
 
 
 if __name__ == "__main__":
-    req = urllib.request.Request('https://intranet.hbtn.io/status')
-    with urllib.request.urlopen(req) as response:
-        the_page = response.read()
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        html = response.read()
         print("Body response:")
-        print("\t- type:", type(the_page))
-        print("\t- content:", the_page)
-        print("\t- uft8 content:", the_page.decode("utf-8"))
+        print("\t- type:", type(html))
+        print("\t- content:", html)
+        print("\t- uft8 content:", html.decode("utf8"))
