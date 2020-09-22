4a119686a5488a611405a2d26995df0c5ffdb682
CrisRuedaP <crisruedap@gmail.com>
2020-09-22 23:31:36 +0000

    Refactoring the code

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -3,18 +3,17 @@ const request = require('request');
 const movieId = process.argv[2];
 const url = 'https://swapi-api.hbtn.io/api/films/' + movieId;
 request(url, function (error, response, body) {
-  if (error) {
-    console.log(error);
-  }
-  const info = JSON.parse(body);
-  for (const character of info.characters) {
-    request(character, function (error, response, body) {
-      const info = JSON.parse(body);
-      if (error) {
-        console.log(error);
-      } else {
-        console.log(info.name);
-      }
-    });
+  if (!error && response.statusCode === 200) {
+    const info = JSON.parse(body);
+    for (const character of info.characters) {
+      request(character, function (error, response, body) {
+        const info = JSON.parse(body);
+        if (error) {
+          console.log(error);
+        } else {
+          console.log(info.name);
+        }
+      });
+    }
   }
 });
