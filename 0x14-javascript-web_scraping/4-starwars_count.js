f933acf1386a6457135fb7999a971e3ede559b21
CrisRuedaP <crisruedap@gmail.com>
2020-09-22 19:57:14 +0000

    Adding task 4

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+let count = 0;
+request(url, function (error, response, body) {
+  if (!error && response.statusCode === 200) {
+    const info = JSON.parse(body);
+    for (const movie of info.results) {
+      for (const character of movie.characters) {
+        if (character.includes('18')) {
+          count++;
+        }
+      }
+    }
+  }
+  console.log(count);
+});
