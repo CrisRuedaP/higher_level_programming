25ad8154a7c63a93c4eb8bfc687e0a6b12629825
CrisRuedaP <crisruedap@gmail.com>
2020-09-22 16:44:40 +0000

    Changing the first character of the variable name

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const fs = require('fs');
 const filePath = process.argv[2];
-const SToWrite = process.argv[3];
-fs.writeFile(filePath, SToWrite, { encoding: 'utf-8' }, function (err, data) {
+const sToWrite = process.argv[3];
+fs.writeFile(filePath, sToWrite, { encoding: 'utf-8' }, function (err, data) {
   if (err) {
     console.log(err);
   }
