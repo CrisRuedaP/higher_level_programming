a25036b2138af57e44aa00dd691792331cb094d7
CrisRuedaP <crisruedap@gmail.com>
2020-09-22 17:00:12 +0000

    fix semistandard style

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const fs = require('fs');
-let filePath = process.argv[2]
+const filePath = process.argv[2];
 fs.readFile(filePath, { encoding: 'utf-8' }, function (err, data) {
   if (err) {
     console.log(err);
