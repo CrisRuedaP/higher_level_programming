4f3376538222d6f5edd201caae7c2dc28850f248
CrisRuedaP <crisruedap@gmail.com>
2020-09-24 17:26:06 +0000

    Adding tasks from 0 to 9

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,5 @@
+$.get('https://swapi-api.hbtn.io/api/films/?format=json', function (data) {
+  for (const movie in data.results) {
+    $('ul#list_movies').append(`<li>${data.results[movie].title}</li>`);
+  }
+});
