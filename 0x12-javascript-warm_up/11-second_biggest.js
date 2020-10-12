1fa1ff1b7502d99fba486477d9f213756c7a3535
CrisRuedaP <crisruedap@gmail.com>
2020-10-12 23:18:26 +0000

    fixed output

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,19 +1,8 @@
 #!/usr/bin/node
-const args = process.argv;
-const splitted = args.splice(2);
-
-function secondLargest (arr) {
-  const sorted = arr.sort(function (a, b) {
-    return a - b;
-  });
-  sorted.pop();
-  const length = sorted.length;
-  if (length > 1) {
-    const result = Number(sorted.pop());
-    return result;
-  } else {
-    return 0;
-  }
+if (process.argv.length < 4) {
+  console.log('0');
+} else {
+  const args = process.argv.slice(2);
+  args.sort((a, b) => b - a);
+  console.log(args[1]);
 }
-
-console.log(secondLargest(splitted));
